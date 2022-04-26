1a668852554e357f56408890cfcfc4e6f7124ae8
CrackerJaps <thecrackerjaps@gmail.com>
2022-04-25 21:37:48 -0700

    unbutton nav buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,37 +9,22 @@ export default function Header(props) {
   const header = (
     <header className="header__container flex-row border-bottom">
       <div className="header__icon-div flex-row">
-        <button
-          className="header__btn"
-          onClick={toggleBobby}
-          disabled={isModalOpen}
-        >
-          <i className="fa-solid fa-bars header__btn-icon"></i>
-        </button>
-        <button
-          className="header__btn"
+        <i className="fa-solid fa-bars header__btn" onClick={toggleBobby}></i>
+        <i
+          className="fa-regular fa-circle-question header__btn"
           onClick={toggleHelp}
-          disabled={isModalOpen}
-        >
-          <i className="fa-regular fa-circle-question header__btn-icon"></i>
-        </button>
+        ></i>
       </div>
       <h1 className="header__title">BOBBY SHMURDLE</h1>
       <div className="header__icon-div flex-row">
-        <button
-          className="header__btn"
+        <i
+          className="fa-solid fa-chart-column header__btn"
           onClick={toggleStats}
-          disabled={isModalOpen}
-        >
-          <i className="fa-solid fa-chart-column header__btn-icon"></i>
-        </button>
-        <button
-          className="header__btn"
+        ></i>
+        <i
+          className="fa-solid fa-gear header__btn"
           onClick={toggleSettings}
-          disabled={isModalOpen}
-        >
-          <i className="fa-solid fa-gear header__btn-icon"></i>
-        </button>
+        ></i>
       </div>
     </header>
   )
